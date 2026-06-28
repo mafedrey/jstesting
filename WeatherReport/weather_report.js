724f72d16c7bbd110f873f8eb5773016aa1ad739
mafedrey <mafe22@yahoo.com>
2026-06-28 17:54:56 -0400

    Weather Report Practise Task

--- WeatherReport/weather_report.js
+++ WeatherReport/weather_report.js
@@ -1,4 +1,4 @@
-function showweatherDetails(event) {
+/*function showweatherDetails(event) {
       event.preventDefault();
 
  const city = document.getElementById('city').value;
@@ -12,5 +12,34 @@ function showweatherDetails(event) {
                                   <p>Temperature: ${data.main.temp} &#8451;</p>
                                   <p>Weather: ${data.weather[0].description}</p>`;
         })
+.catch(error => {
+          console.error('Error fetching weather:', error);
+          const weatherInfo = document.getElementById('weatherInfo');
+          weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
+        });
 }
- document.getElementById('weatherForm').addEventListener('submit',showweatherDetails );
+*/
+function weatherwithCoordinates(event) {
+      event.preventDefault();
+
+ const lat = document.getElementById('lat').value;
+ const lon = document.getElementById('lon').value;
+      const apiKey = 'b3cc4c591cc63588eea9cd3d4f307c61'; // Replace 'YOUR_API_KEY' with your actual API key
+      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+ fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+          const weatherInfo = document.getElementById('weatherInfo');
+          weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+                                  <p>Temperature: ${data.main.temp} &#8451;</p>
+                                  <p>Weather: ${data.weather[0].description}</p>`;
+        })
+.catch(error => {
+          console.error('Error fetching weather:', error);
+          const weatherInfo = document.getElementById('weatherInfo');
+          weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
+        });
+}
+
+ //document.getElementById('weatherForm').addEventListener('submit',showweatherDetails );
+ document.getElementById('latLong').addEventListener('submit',weatherwithCoordinates );
